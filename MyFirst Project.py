6bda759c4e0afd1ff23883d41f4087702394cf44
sswcode <ssw@3xn.dk>
2023-01-12 15:47:26 +0100

    Added first parts of data

--- MyFirst Project.py
+++ MyFirst Project.py
@@ -2,5 +2,5 @@ print ("hello world")
 
 print ("here")
 
-import pandas as pd
-pd.DataFrame([[0,12,3],[0,12,3]] )
+
+print ("here")
--- test.py
+++ test.py
@@ -4,5 +4,6 @@ print (os.getcwd())
 print (2*3)
 
 import pandas as pd
-#import numpy as numpy
-#pd.DataFrame([[0,12,3],[0,12,3]] )
+import numpy as numpy
+frame = pd.DataFrame([[0,12,3],[0,12,3]] )
+print (frame)
