05fbc75928d8b55a3f7c1fbe7f2b99542f676b3a
sswcode <ssw@3xn.dk>
2023-01-12 14:17:29 +0100

    first commit

--- MyFirst Project.py
+++ MyFirst Project.py
@@ -1,3 +1,6 @@
 print ("hello world")
 
 print ("here")
+
+import pandas as pd
+pd.DataFrame([[0,12,3],[0,12,3]] )
---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+print ("current directory")
+print (os.getcwd())
+print (2*3)
+
+import pandas as pd
+#import numpy as numpy
+#pd.DataFrame([[0,12,3],[0,12,3]] )
